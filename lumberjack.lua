f970d6d72fecaa54a78ed592968ceb3078aefda4
Alexnortung <alex_nortung@live.dk>
2019-10-14 21:42:13 +0200

    can go back and forth.

--- lumberjack.lua
+++ lumberjack.lua
@@ -1,8 +1,9 @@
 -- this program will cut down trees that are lined up in a straight line
-local treeDistance = 5
-local trees = 5
+local treeDistance = 4 -- blocks between trees
+-- local trees = 5
 local x, y, z = 0, 0, 0 -- relative coordinates for the turtle
 local facingDirection = 0 -- 0 is towards positive x
+local state = 0 -- 0 cutting trees; 1 planting; 2 waiting
 
 function ternary ( cond , T , F )
     if cond then return T else return F end
@@ -37,6 +38,19 @@ local function forward(steps)
     end
 end
 
+local function back(steps)
+    steps = steps or 1
+    for i = 1, steps do
+        turtle.back()
+    end
+    local addSteps = ternary(facingDirection >= 2, -steps, steps)
+    if facingDirection % 2 == 0 then
+        x = x - addSteps
+    else
+        z = z - addSteps
+    end
+end
+
 local function up(steps)
     steps = steps or 1
     for i = 1, steps do
@@ -67,11 +81,12 @@ local function cutTree()
 
     if fSuccess then
         -- if sapliung move around
-        if fData.name == "oak_sapling" then
-            goAbove()
+        if (fData.name == "minecraft:oak_sapling") or (fData.name == "minecraft:birch_sapling") then
+            -- goAbove()
+            state = 1
         else
             -- dig the bootom and move under the other logs
-            print("tree detected")
+            -- print("tree detected")
             local treeFinished = false
             turtle.dig()
             forward()
@@ -82,7 +97,7 @@ local function cutTree()
                 up()
                 local success, data = turtle.inspectUp()
                 print(success, data.name)
-                if success and (data.name == "oak_log" or data.name == "minecraft:birch_log") then
+                if success and (data.name == "minecraft:oak_log" or data.name == "minecraft:birch_log") then
                     --continue
                 else
                     treeFinished = true
@@ -96,4 +111,39 @@ local function cutTree()
     end
 end
 
-cutTree()
+local function decider()
+    local fSuccess, fData = turtle.inspectDown()
+    if state == 0 then
+        -- check down
+        -- check forward
+        if fData.name == "minecraft:cobblestone" then
+            -- change state
+            state = 1
+            back()
+        elseif turtle.detect() then
+            print("detected")
+            cutTree()
+        else
+            forward()
+        end
+    elseif state == 1 then
+        -- select sapling
+        if fData.name == "minecraft:cobblestone" then
+            state = 2
+        elseif x % treeDistance == 0 then
+            -- plant sapling
+        end
+        
+        if state ~= 2 then
+            back()
+        end
+    end
+end
+
+forward()
+while state ~= 2 do
+    decider()
+end
+
+
+-- cutTree()
